834f7d42dea1840f8d8fc4c2c60a6b8e778993ae
Martí Ejarque Galindo <marti.ejarque@students.salle.url.edu>
2021-04-28 17:04:40 +0200

    store js

---
+++ public/assets/js/store.js
@@ -0,0 +1,28 @@
+function compra(gameID, title, salePrice, storeID, dealRating, thumb){
+    let params = {
+        title: title,
+        salePrice: salePrice,
+        storeID: storeID,
+        dealRating: dealRating,
+        thumb: thumb
+    };
+
+
+    const form = document.createElement('form');
+    form.method = 'POST';
+    form.action = '/store/buy/'+gameID;
+
+    for (const key in params) {
+        if (params.hasOwnProperty(key)) {
+            const hiddenField = document.createElement('input');
+            hiddenField.type = 'hidden';
+            hiddenField.name = key;
+            hiddenField.value = params[key];
+
+            form.appendChild(hiddenField);
+        }
+    }
+
+    document.body.appendChild(form);
+    form.submit();
+}
